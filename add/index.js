f15fcb6c34b2d8ac93b89aa1bddb31907a4a1cf3
yadav.sanjay <yadav.sanjay@naukri.com>
2020-06-07 09:59:09 +0530

    fixed issue...........

--- add/index.js
+++ add/index.js
@@ -1,3 +1,3 @@
 const add = require('./add');
 
-module.exports = { add: add };
+module.exports = add;
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const add = require('./add');
 const subtract = require('./subtract');
 
+
+console.log(add(19,20))
 module.exports = { add: add, subtract: subtract };
